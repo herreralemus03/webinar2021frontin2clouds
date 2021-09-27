f768b48a504db11189a66d67259d87b72bd827af
Herreralemus03 <herreralemus.03@gmail.com>
2021-09-27 02:23:16 -0600

    beauty

--- lib/providers/server_config.dart
+++ lib/providers/server_config.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart';
 
-const host = "localhost:8080"; // "webinar2021-aws.herokuapp.com";
+const host = /*"localhost:8080";*/ "webinar2021-aws.herokuapp.com";
 //const port = 8080;
 
 Future<Response> doGet(String collection, {Map<String, String>? params}) async {
   try {
-    final uri = Uri.http(host, collection, params);
+    final uri = Uri.https(host, collection, params);
     final response = await get(uri, headers: {
       'Content-type': 'application/json',
       'Accept': 'application/json',
@@ -20,7 +20,7 @@ Future<Response> doGet(String collection, {Map<String, String>? params}) async {
 Future<Response> doGetCustom(String url, String collection,
     {Map<String, dynamic>? params}) async {
   try {
-    final uri = Uri.http(url, collection, params);
+    final uri = Uri.https(url, collection, params);
     final response = await get(uri, headers: {
       'Content-type': 'application/json',
       'Accept': 'application/json',
@@ -34,7 +34,7 @@ Future<Response> doGetCustom(String url, String collection,
 Future<Response> doPost(String collection,
     {required Map<String, dynamic> body, Map<String, String>? params}) async {
   try {
-    final uri = Uri.http(host, collection, params);
+    final uri = Uri.https(host, collection, params);
     final response = await post(uri, body: json.encode(body), headers: {
       'Content-type': 'application/json',
       'Accept': 'application/json',
